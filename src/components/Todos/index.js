8554a4c78281ae20805142f3166d4862ebad5eb4
Mamta <mamtaanand08@gmail.com>
2024-09-19 12:16:02 +0530

    changes

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -1,32 +1,85 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// const Todos = (props) => {
+
+//     const [todos, setTodos] = useState([])
+
+//     useEffect(() => {
+//         fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json()).then(data => {
+//             setTodos(data)
+//         })
+//     }, [])
+
+//     return (
+//         <>
+//             <div id="myDIV">
+//                 {
+//                     todos && todos.map((el, index) => (
+//                         <>                        
+//                             <h5 key={index}>My To Do List {el.title} </h5>
+//                             {el.completed ? (<p style={{ color: 'green' }}>Completed</p>) : (<p style={{ color: 'red' }}>Not Completed</p>)}
+//                         </>
+//                     ))
+//                 }
+//             </div>
+
+
+//         </>
+//     )
+// }
+// export default Todos;
+
+
+import { useState, useEffect } from "react";
 
 const Todos = (props) => {
+  const [todos, setTodos] = useState('')
 
-    const [todos, setTodos] = useState([])
-    
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json()).then(data => {
-            setTodos(data)
-        })
-    }, [])
-   
+  useEffect(() => {
+ 
+    const storedTodos = localStorage.getItem('todos');
 
-    return (
-        <>
-            <div id="myDIV">
-                {
-                    todos && todos.map((el, index) => (
-                        <>                        
-                            <h5 key={index}>My To Do List {el.title} </h5>
-                            {el.completed ? (<p style={{ color: 'green' }}>Completed</p>) : (<p style={{ color: 'red' }}>Not Completed</p>)}
-                        </>
-                    ))
-                }
-            </div>
+    if (storedTodos) {
+  
+      let list = JSON.parse(storedTodos);
+      let updatedList = list.map(item => ({
+        ...item,
+        title: 'New Title' 
+      }));
+      setTodos(updatedList);
+      localStorage.setItem('todos', JSON.stringify(updatedList));
+    } else {
+ 
+      const defaultTodos = [
+        { id: 1, title: 'List1', completed: false },
+        { id: 2, title: 'List2', completed: false },
+        { id: 3, title: 'Final List', completed: true }
+      ];
+      setTodos(defaultTodos);
+      localStorage.setItem('todos', JSON.stringify(defaultTodos));
+    }
+  }, []);
+  console.log("todos",todos)
 
-
-        </>
-    )
+  return (
+    <>
+      <div id="myDIV">
+        {
+          todos && todos.map((el, index) => (
+            <>
+              <div key={el.id}>
+                <h5>My To Do List: {el.title}</h5>
+                {el.completed ? (
+              <p style={{ color: 'green' }}>Completed</p>
+            ) : (
+              <p style={{ color: 'red' }}>Not Completed</p>
+            )}                
+              </div>
+              </>
+          ))
+        }
+      </div>
+    </>
+  )
 }
 export default Todos;
-
