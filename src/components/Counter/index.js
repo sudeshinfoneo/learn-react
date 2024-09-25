81b384965fdcd8d833b94aa6b480da94a50a0f8b
Sudesh <sudesh.kumar@infofoneotech.com>
2024-09-25 13:37:54 +0530

    added

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,20 +1,76 @@
 import { Link } from "react-router-dom";
 import React, { useState, useCallback } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../../store/features/counter/counterSlice";
+import { addTodo } from "../../store/features/todo/todoSlice";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [formData, setFormData] = useState({});
   const [otherState, setOtherState] = useState(false);
+  const { value } = useSelector((store) => store.counter)
+  const {todos} = useSelector((store) => store.todo)
+  const dispatch = useDispatch()
 
-  const increment = useCallback(() => {
-    setCount((prevCount) => prevCount + 1);
-  }, []);
+  const handleinputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleincrement = () => {
+    dispatch(increment())
+  }
+
+  const handledecrement = () => {
+    dispatch(decrement())
+  }
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    dispatch(addTodo(formData))
+  };
 
   return (
     <div className="container mt-5">
-      <h3 className="display-4"><b>Count: {count}</b></h3>
-      <button className="btn btn-primary mr-2" onClick={increment}><b>Increment</b></button>
-      <button className="btn btn-secondary" onClick={() => setOtherState(!otherState)}>
-        <b>Toggle Other State</b></button>
+      <h3 className="display-4"><b>Count: {value}</b></h3>
+      <button className="btn btn-primary mr-2" onClick={(e) => handleincrement(e)}><b>Increment</b></button>
+      <button className="btn btn-primary mr-2" onClick={(e) => handledecrement(e)}><b>Decrement</b></button>
+      <hr />
+      <div>
+        <div>
+          <input name="fname" onChange={(e) => handleinputChange(e)} placeholder="First Name" />
+          <input name="lname" onChange={(e) => handleinputChange(e)} placeholder="Last Name" />
+          <input name="haddle" onChange={(e) => handleinputChange(e)} placeholder="Haddle" />
+          <button onClick={(e) => handleAdd(e)}>Add</button>
+        </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              todos && todos.map((el, ind) => (
+                <tr key={ind}>
+                  <th scope="row">{ind + 1}</th>
+                  <td>{el.fname}</td>
+                  <td>{el.lname}</td>
+                  <td>{el.haddle}</td>
+                </tr>
+              
+              ))
+            }
+            
+            
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
